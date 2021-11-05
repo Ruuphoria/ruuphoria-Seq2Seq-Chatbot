957c4182e67b268e3068c19520fa11409bbc7a50
Ruuphoria <ruuphoria@proton.me>
2021-11-05 21:04:14 +0000

    Add bower config

--- grucell_cond.py
+++ grucell_cond.py
@@ -2,3 +2,8 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
 from tensorflow.python.framework import ops
+from tensorflow.python.ops.rnn_cell_impl import _RNNCell as RNNCell
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import variable_scope as vs
+from tensorflow.python.ops.math_ops import tanh, sigmoid
+from tensorflow.contrib.rnn.python.ops.core_rnn_cell_impl import _linear, _checked_scope
