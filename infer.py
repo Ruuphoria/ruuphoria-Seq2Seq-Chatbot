17ed534dd748a77249c5401246e52d4f408d7671
Ruuphoria <ruuphoria@proton.me>
2022-10-15 10:57:33 +0000

    add emoji

--- infer.py
+++ infer.py
@@ -13,4 +13,4 @@ checkpoint = "/tmp/model.ckpt"
 pp = pprint.PrettyPrinter()
 
 flags = tf.app.flags
-flags.DEFINE_string("checkpoint", "checkpoint", "D
+flags.DEFINE_string("checkpoint", "checkpoint", "Dire
