3c9f04de00ee4a04f2bc6695fc8f204295c25a56
Ruuphoria <ruuphoria@proton.me>
2022-11-20 18:03:18 +0000

    release: minify javascript resources

--- infer.py
+++ infer.py
@@ -14,4 +14,5 @@ pp = pprint.PrettyPrinter()
 
 flags = tf.app.flags
 flags.DEFINE_string("checkpoint", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
-flags.DEFINE_string("logdir", "log", "Log directory [log
+flags.DEFINE_string("logdir", "log", "Log directory [log]")
+f
