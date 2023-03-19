ff92ec0c3cdfd172e9222e13b638d1de38846c24
Ruuphoria <ruuphoria@proton.me>
2023-03-19 22:02:04 +0000

    Fixed small issues

--- infer.py
+++ infer.py
@@ -27,4 +27,4 @@ def main(_):
     emb = np.load("./data/emb.npy")
     chars = cPickle.load(open("./data/vocab.pkl", 'rb'))
     vocab_size, emb_size = np.shape(emb)
-    data_loader = TextLoader('./data', 1, c
+    data_loader = TextLoader('./data', 1, cha
