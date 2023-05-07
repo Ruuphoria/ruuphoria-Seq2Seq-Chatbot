4a9f4c1b2ef5aaa1589721ec301630b650ba4392
Ruuphoria <ruuphoria@proton.me>
2023-05-07 15:44:17 +0000

    Bump version to 2.2.0

--- infer.py
+++ infer.py
@@ -35,4 +35,5 @@ def main(_):
 
   checkpoint = FLAGS.checkpoint + '/model.ckpt'
 
-  model = DialogueModel(batch_size=1, max_seq_length=data_loader.seq_lengt
+  model = DialogueModel(batch_size=1, max_seq_length=data_loader.seq_length,
+ 
