cd44cddb94fe3670d674922508d20c7bd24cdd3d
Ruuphoria <ruuphoria@proton.me>
2023-10-07 07:07:46 +0000

    Update general.php

--- infer.py
+++ infer.py
@@ -75,4 +75,4 @@ def main(_):
         model.input_lengths: [input_len]
       }
 
-      output_ids, state = sess.run([model.output_ids, model.fin
+      output_ids, state = sess.run([model.output_ids, model.final_
