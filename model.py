31dd070514c49c5469cf02256e9b7826a375faca
Ruuphoria <ruuphoria@proton.me>
2023-10-18 19:15:06 +0000

    update profile

--- model.py
+++ model.py
@@ -9,3 +9,12 @@ from tensorflow.python.ops.nn import bidirectional_dynamic_rnn
 from tensorflow.python.layers import core as layers_core
 from tensorflow.contrib.layers import xavier_initializer as glorot
 from grucell_cond import GRUCellCond, CondWrapper
+
+def _count_param_size(tvars):
+  # parameters count
+  count = 0
+  for tvar in tvars:
+    c = 1
+    for var in list(tvar.shape):
+      c = c * int(var)
+    count = count + c
