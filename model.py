e5e5c1154792353e01d766fa5b6e191e6b6202d9
Ruuphoria <ruuphoria@proton.me>
2023-11-08 13:20:03 +0000

    Added a comment

--- model.py
+++ model.py
@@ -192,3 +192,4 @@ class DialogueModel(object):
                "final_state": self.final_state,
                "loss": self.loss,
                "global_step": self.global_step,
+               "train_op": self.train_op}
