b57dc8ec454b2e5b4413f2314f569dda76cb7477
Ruuphoria <ruuphoria@proton.me>
2023-11-10 14:23:31 +0000

    Involve Jenkins

--- train.py
+++ train.py
@@ -6,3 +6,10 @@ import _pickle as cPickle
 from glob import glob
 import math
 import sys
+import numpy as np
+from utils import TextLoader, UNK_ID
+from model import DialogueModel
+
+pp = pprint.PrettyPrinter()
+
+flags = tf.app.flags
